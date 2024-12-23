fd5c76c11e9fdc3d8e75d9dcf743e10bc257142e
BlauesMonster <blauesmonsterdaraw@gmail.com>
2024-12-23 02:53:33 +0100

    .

--- src/app/snow-globe/snow-globe.component.ts
+++ src/app/snow-globe/snow-globe.component.ts
@@ -1,23 +1,36 @@
-import { Component, HostListener } from '@angular/core';
-function randomIntFromInterval(min:number, max:number) { // min and max included
+import { AfterViewInit, ChangeDetectionStrategy, Component, computed, ElementRef, HostListener, signal, viewChild, ViewChild } from '@angular/core';
+function randomIntFromInterval(min: number, max: number) { // min and max included
   return Math.floor(Math.random() * (max - min) + min);
 }
 @Component({
   selector: 'app-snow-globe',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './snow-globe.component.html',
   styleUrls: ['./snow-globe.component.scss'],
 })
 export class SnowGlobeComponent {
-  canvas!: HTMLCanvasElement;
-  ctx!: CanvasRenderingContext2D;
+
+
   snowParticles: { x: number; y: number; speed: number; state: boolean; angle: number; rotationSpeed: number }[] = [];
   maxSnowflakes = 7000; // Limit the number of snowflakes
-  groundLevel = 0; // Dynamically calculated
   snowPile: Map<number, number> = new Map(); // Tracks accumulated snow at specific x positions
 
+  canvas = viewChild.required<ElementRef<HTMLCanvasElement>>("myCanvas");
+
+  ctx = computed(() => {
+    const canvas = this.canvas();
+    return canvas.nativeElement.getContext('2d')!
+  });
+
+  canvasWidth = signal(window.innerWidth)
+  canvasHeight = signal(window.innerHeight)
+  groundLevel = computed(() => {
+    const height = this.canvasHeight();
+    return height;
+  });
+
   ngOnInit() {
-    this.initCanvas();
     this.addSnowParticles();
     this.animate();
 
@@ -25,23 +38,12 @@ export class SnowGlobeComponent {
     window.addEventListener('devicemotion', this.handleShake.bind(this));
   }
 
-  initCanvas() {
-    this.canvas = document.createElement('canvas');
-    this.canvas.style.backgroundColor = 'black';
-    this.ctx = this.canvas.getContext('2d')!;
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-    document.body.appendChild(this.canvas);
-
-    // Set the ground level based on the canvas height
-    this.groundLevel = this.canvas.height;
-  }
 
   addSnowParticles() {
     for (let i = 0; i < this.maxSnowflakes; i++) {
       this.snowParticles.push({
-        x: Math.random() * this.canvas.width,
-        y: Math.random() * this.canvas.height - this.canvas.height,
+        x: Math.random() * this.canvasWidth(),
+        y: Math.random() * this.canvasHeight() - this.canvasHeight(),
         speed: 0.5 + Math.random() * 1.5,
         state: true,
         angle: Math.random() * 2 * Math.PI, // Random angle for twisting
@@ -53,7 +55,7 @@ export class SnowGlobeComponent {
   animate() {
     requestAnimationFrame(() => this.animate());
 
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx().clearRect(0, 0, this.canvas().nativeElement.width, this.canvas().nativeElement.height);
 
     for (let i = 0; i < this.snowParticles.length; i++) {
       const particle = this.snowParticles[i];
@@ -63,7 +65,7 @@ export class SnowGlobeComponent {
         particle.x += Math.sin(particle.angle) * 0.5; // Simulate horizontal drifting
         particle.angle += particle.rotationSpeed; // Update angle for twisting effect
 
-        const targetHeight = this.groundLevel - this.getPileHeight(particle.x);
+        const targetHeight = this.groundLevel() - this.getPileHeight(particle.x);
 
         if (particle.y >= targetHeight) {
           particle.y = targetHeight;
@@ -71,14 +73,14 @@ export class SnowGlobeComponent {
           this.addToPile(particle.x);
         }
 
-        this.ctx.fillStyle = 'white';
-        this.ctx.fillRect(particle.x, particle.y, 4, 4);
+        this.ctx().fillStyle = 'white';
+        this.ctx().fillRect(particle.x, particle.y, 4, 4);
       }
     }
 
     this.snowPile.forEach((value, key) => {
-      this.ctx.fillStyle = 'white';
-      this.ctx.fillRect(key, this.canvas.height, 4, -value);
+      this.ctx().fillStyle = 'white';
+      this.ctx().fillRect(key, this.canvasHeight(), 4, -value);
     });
   }
 
@@ -131,34 +133,22 @@ export class SnowGlobeComponent {
   modifyParticleDynamics() {
     // Apply randomized velocity shifts to simulate a realistic twist effect
     for (const particle of this.snowParticles) {
-      const velocityChangeX = (Math.random() - 0.5) * 4; // Random horizontal velocity
-      const velocityChangeY = (Math.random() - 0.5) * 4; // Random vertical velocity
 
       particle.speed = 0.5 + Math.random() * 1.5; // Randomize falling speed
       particle.angle = Math.random() * 2 * Math.PI; // Randomize twisting direction
       particle.rotationSpeed = 0.02 + Math.random() * 0.05; // Randomize rotation speed
 
       // Adjust particle position slightly to simulate shaking motion
-      particle.x = randomIntFromInterval(0, this.canvas.width);
-      particle.y =randomIntFromInterval(0, this.canvas.height);
+      particle.x = randomIntFromInterval(0, this.canvasWidth());
+      particle.y = randomIntFromInterval(0, this.canvasHeight());
 
       // Keep particles within the canvas bounds
       if (particle.x < 0) particle.x = 0;
-      if (particle.x > this.canvas.width) particle.x = this.canvas.width;
+      if (particle.x > this.canvasWidth()) particle.x = this.canvasWidth();
       if (particle.y < 0) particle.y = 0;
-      if (particle.y > this.canvas.height) particle.y = this.canvas.height;
+      if (particle.y > this.canvasHeight()) particle.y = this.canvasHeight();
 
       particle.state = true; // Keep particles active
     }
-  }
-
-
-
-  @HostListener('window:resize')
-  onWindowResize() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-
-    this.groundLevel = this.canvas.height;
   }
 }
